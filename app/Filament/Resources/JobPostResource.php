2d88a8ebf6f8955f853adcc6bd2372de4ea15ca3
Foulla <samankassoufoulla@gmail.com>
2024-02-04 14:01:05 +0100

    Add 'job posts'

---
+++ app/Filament/Resources/JobPostResource.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\JobPostResource\Pages;
+use App\Filament\Resources\JobPostResource\RelationManagers;
+use App\Models\JobPost;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class JobPostResource extends Resource
+{
+    protected static ?string $model = JobPost::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('title')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Textarea::make('description')
+                    ->required()
+                    ->maxLength(65535),
+                Forms\Components\FileUpload::make('banner')
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('title'),
+                Tables\Columns\TextColumn::make('description')
+                    ->limit(100),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListJobPosts::route('/'),
+            'create' => Pages\CreateJobPost::route('/create'),
+            'edit' => Pages\EditJobPost::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Models/JobPost.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class JobPost extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'is_published',
+        'banner'
+    ];
+
+    protected $casts = [
+        'is_published' => 'boolean',
+    ];
+}
