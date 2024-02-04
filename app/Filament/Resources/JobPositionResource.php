2c55c742e1b5cb172dee042faa8bf746dc701232
Foulla <samankassoufoulla@gmail.com>
2024-02-04 10:58:49 +0100

    Add Job positions crud

---
+++ app/Filament/Resources/JobPositionResource.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\JobPositionResource\Pages;
+use App\Filament\Resources\JobPositionResource\RelationManagers;
+use App\Models\JobPosition;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class JobPositionResource extends Resource
+{
+    protected static ?string $model = JobPosition::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('title')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Textarea::make('description')
+                    ->maxLength(65535)
+                    ->columnSpan('full')
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('title'),
+                Tables\Columns\TextColumn::make('description')
+                    ->limit(100),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListJobPositions::route('/'),
+            'create' => Pages\CreateJobPosition::route('/create'),
+            'edit' => Pages\EditJobPosition::route('/{record}/edit'),
+        ];
+    }
+}
